d17b578a285ea2636e7216167b714ffd15865840
Ryan Dean <ryan@mobalyticshq.com>
2018-05-18 16:25:06 -0700

    Updated code to work with websockets

--- server/app.js
+++ server/app.js
@@ -17,15 +17,55 @@ const io = socket(server)
 
 let vCap = null
 const devicePort = 0
+let calibrated = false
 
 io.on('connection', function(socket){
     console.log('a user connected')
     socket.on('disconnect', () => {
         console.log('user disconnected');
-      });
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
     });
+    
+    socket.on('img', (data) => {
+        const base64text= data//Base64 encoded string
+        const base64data = base64text.replace('data:image/jpeg;base64','')
+                                    .replace('data:image/png;base64','');//Strip image type prefix
+        const buffer = Buffer.from(base64data,'base64');
+        const image = cv.imdecode(buffer); //Image is now represented as Mat
+        io.emit('frame', data)
+        if (!calibrated) {
+            image.findChessboardCornersAsync(new cv.Size(12,12)).then((corners) => {
+                if (corners && corners.corners.length > 0) { 
+                    io.emit('calibration', corners.corners)
+                    calibrated = true
+                    console.log(calibrated)
+                }
+            })
+        } else {
+            const canvas = new Canvas(image.cols, image.rows)
+            const ctx = canvas.getContext('2d')
+
+            // convert your image to rgba color space
+            const matRGBA = image.channels === 1
+            ? image.cvtColor(cv.COLOR_GRAY2RGBA)
+            : image.cvtColor(cv.COLOR_BGR2RGBA);
+
+            // console.log(image)
+            // create new ImageData from raw mat data
+            const imgData = new Canvas.ImageData(
+                new Uint8ClampedArray(matRGBA.getData()),
+                    image.cols,
+                    image.rows
+            );
+
+            // build an aruco detector
+            const detector = new AR.AR.Detector();
+            const markers = detector.detect(imgData)
+            if (markers.length > 0)  {
+                io.emit('tile-movement', markers)
+                console.log('found')
+            }
+        }
+    })
 
     socket.on('calibrate', () => {
         // open capture from webcam
@@ -112,8 +152,10 @@ io.on('connection', function(socket){
     })
 })
 
-server.listen(3000, (err) => {
+const PORT = process.env.PORT || 8000
+const HOST = '0.0.0.0'
+
+server.listen(PORT, HOST, (err) => {
     if (err) console.error(err)
-    else console.log(`Server listening on ${process.env.PORT || 3000}`) 
-    if (!vCap) vCap = new cv.VideoCapture(devicePort);
+    else console.log(`Server listening on ${PORT}`) 
 });
